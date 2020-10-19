671cb81119dbad3e354bcfa675dbb3bcf65a4af1
VasiliyMac <cartario@yandex.ru>
2020-10-19 23:46:48 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get('/test', (req, res)=>{  
+  res.send({
+    status: 'ok'
+  });  
+});
+
+app.listen(PORT, ()=>{
+  console.log(`SERVER RUNNING ON PORT ${PORT}`);
+});
