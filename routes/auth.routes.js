ba4859de75edee2d4ffca68855a2285f20f7e484
VasiliyMac <cartario@yandex.ru>
2020-10-21 23:33:27 +0300

    register, validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,24 @@
 const {Router} = require('express');
 const router = Router();
 const UserModel = require('../models/User');
+const bcrypt = require('bcryptjs');
+const {check, validationResult } = require('express-validator');
 
-console.log(UserModel);
+router.post('/register', 
+  [
+    check('email', 'Неверный email').isEmail().exists(),
+    check('password', 'Минимальная длина пароля не менее 6 символов').isLength({min: 6}).exists()
+  ], async (req, res)=>{
+    
+  const {email, password} = req.body;
 
-router.post('/register', async (req, res)=>{
-  const {email, password} = req.body; 
   try {
+    const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ status: 'error', errors: errors.array()});
+        return;
+      }
+
     const candidate = await UserModel.findOne({email});
     
     if(candidate){
@@ -15,11 +27,15 @@ router.post('/register', async (req, res)=>{
       }); 
       return;     
     }
-    const data = {email, password};
+    const hashedPassword = await bcrypt.hash(password, 12);
+    
+    const data = {email, password: hashedPassword};
 
     const user = await UserModel.create(data);
     
-    res.status(200).json(user);    
+    res.status(200).json({
+      message: 'Пользователь создан'      
+    });    
   }
   catch(err){    
     res.status(500).send();
