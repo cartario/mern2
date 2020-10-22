36424c217f82610baaa14d7fc44428ebcf3d4f9c
VasiliyMac <cartario@yandex.ru>
2020-10-22 11:53:20 +0300

    login, jwt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,14 +2,17 @@ const {Router} = require('express');
 const router = Router();
 const UserModel = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const {check, validationResult } = require('express-validator');
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 router.post('/register', 
   [
     check('email', 'Неверный email').isEmail().exists(),
     check('password', 'Минимальная длина пароля не менее 6 символов').isLength({min: 6}).exists()
   ], async (req, res)=>{
-    
+
   const {email, password} = req.body;
 
   try {
@@ -42,8 +45,49 @@ router.post('/register',
   }
 });
 
-router.post('/login', async  (req, res)=>{
+router.post('/login', [
+  check('email', 'Неверный email').isEmail().exists(),
+  check('password', 'Минимальная длина пароля не менее 6 символов').isLength({min: 6}).exists()
+], async  (req, res)=>{
 
+  const {email, password} = req.body;
+
+  try {
+    const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ status: 'error', errors: errors.array()});
+        return;
+      }
+    
+    const candidate = await UserModel.findOne({email});
+
+    if(!candidate){
+      res.status(400).send({
+        message: 'Пользователь не найден'
+      });
+      return; 
+    }
+
+    const isMatch = await bcrypt.compare(password, candidate.password);
+    
+    if(!isMatch){
+      return res.status(400).send({message: 'пароли не совпадают'});
+    }
+
+    const token = jwt.sign({userId: candidate.id}, SECRET_KEY, {expiresIn: '1h'});
+
+    const data = {
+      userId: candidate.id,
+      email: candidate.email,
+      token
+    }
+
+    res.send(data);
+
+  }
+  catch(err){
+    res.status(500).send();
+  }
 }); 
 
 module.exports = router;
